07073178007dee32ac35156c5a2d23c088a791bf
Benjamin Lin <blin432@gmail.com>
2019-11-18 22:40:49 -0500

    started to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  template: `
-    <!-- header -->
-    <app-header></app-header>
-    <!-- routes will be rendered here -->
-    <router-outlet></router-outlet>
-    <!-- footer -->
-    <app-footer></app-footer>
-  `,
-  styles: []
-})
-export class AppComponent {}
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <!-- header -->
+//     <app-header></app-header>
+//     <!-- routes will be rendered here -->
+//     <router-outlet></router-outlet>
+//     <!-- footer -->
+//     <app-footer></app-footer>
+//   `,
+//   styles: []
+// })
+// export class AppComponent {}
+
+export class AppComponent { todoArray=[]
+  addTodo(value){    this.todoArray.push(value)} }
