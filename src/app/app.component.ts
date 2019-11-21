e3e45f2de3bf3e0cb54c94fa77ab8b90f03cbf27
Benjamin Lin <blin432@gmail.com>
2019-11-20 22:18:58 -0500

    todo list progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,12 @@
 // export class AppComponent {}
 
 export class AppComponent { todoArray=[]
-  addTodo(value){    this.todoArray.push(value)} }
+  addTodo(value){    this.todoArray.push(value)} 
+  // deleteItem(){   console.log("delete item")  }
+ deleteItem(todo){   for(let i=0 ;i<= this.todoArray.length ;i++){    if(todo== this.todoArray[i]){     this.todoArray.splice(i,1)    }   }  }
+// submit Form  todoSubmit(value:any){ console.log(value)  }
+// submit Form  todoSubmit(value:any){     if(value!==""){    this.todoArray.push(value.todo)     //this.todoForm.reset()    }else{      alert('Field required **')    }      }
+
+}
+
+
